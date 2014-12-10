2c23daaf63feccf0e7a52b75a07fdced9aab7688
Arun Kumar T K <aruntknambiar@gmail.com>
2014-12-11 01:37:32 +0530

    initial commit v 0.0.1

---
+++ README.md
@@ -0,0 +1,12 @@
+#Documentaion
+
+[Polymer Docs](http://polymer-project.org)!
+
+##Source
+ [aruntk:polymer](http://github.com/arunhedcet/polymer) package.
+
+##How to Install
+
+```terminal
+meteor add aruntk:polymer
+```
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+	summary: "Google polymer for material design",
+	version: "0.0.1",
+	git: "https://github.com/arunhedcet/polymer.git",
+	name: 'aruntk:polymer'
+});
+
+Npm.depends({
+	bower: '1.3.12'
+});
+
+Package.onUse(function(api) {
+	api.use('webapp', 'server');
+  	api.use(['templating'], 'client');
+  	api.use(['underscore'], ['client', 'server']);
+	api.versionsFrom('METEOR@0.9.0');
+	api.addFiles('createfiles.js', 'server');
+	api.addFiles('importfiles.html', 'client');
+});
